1a2417ed95dd07d65e2241433a1ad5ac9eb505e8
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-02-05 22:50:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,8 @@ end
 def update_counting_hash(hash, key)
   count = 0 
   if hash[key] 
-    puts hash += 1 
-  else puts 
+    hash[key] += 1 
+  else  
     1
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
